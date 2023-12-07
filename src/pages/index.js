4ac8eb87aaeec4193d7eccd790ca4bce5291aa90
StrixROX <40167899+StrixROX@users.noreply.github.com>
2023-12-08 05:24:38 +0530

    Added data visualization using Recharts (revisit)

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import io from 'socket.io-client'
+import { LineChart, Line, XAxis, YAxis, Legend, Label, ResponsiveContainer } from 'recharts'
 
 import styles from '@/styles/Home.module.css'
 
@@ -11,11 +12,16 @@ function delay(duration) {
   })
 }
 
+function validate(data) {
+  return typeof (data?.time) === 'number' && typeof (data?.data) === 'number' && typeof (data?.sensorId) === 'string'
+}
+
 let socket = null
+const windowSize = 200
 
 export default function Home() {
   const [connectionStatus, setConnectionStatus] = useState("Not connected")
-  const [dataStream, setDataStream] = useState([])
+  const [groupedData, setGroupedData] = useState({})
 
   function connect() {
     setConnectionStatus("Connecting...")
@@ -32,7 +38,32 @@ export default function Home() {
           })
 
           socket.on('message', (message) => {
-            setDataStream(prev => [...prev, message])
+            if (validate(message)) {
+              setGroupedData(prev => {
+                const newGroups = { ...prev }
+
+                if (!newGroups[message.sensorId]) {
+                  newGroups[message.sensorId] = {
+                    startTime: message.time,
+                    data: [message]
+                  }
+                }
+                else {
+                  message['elapsedTime'] = (message.time - newGroups[message.sensorId].startTime) / 1000
+
+                  let newData = [...newGroups[message.sensorId].data]
+                  newData.push(message)
+
+                  if (newData.length > windowSize) {
+                    newData = newData.slice(1)
+                  }
+
+                  newGroups[message.sensorId].data = newData
+                }
+
+                return newGroups
+              })
+            }
           })
 
           socket.on('disconnect', () => {
@@ -51,8 +82,31 @@ export default function Home() {
       <h1 className={styles.heading}>DAQ Platform</h1>
       <p>{connectionStatus}</p>
       <button onClick={() => socket?.disconnect() || connect()}>connect/disonnect</button>
-      <button onClick={() => setDataStream([])}>clear</button>
-      <pre>{JSON.stringify(dataStream, null, 2)}</pre>
+      <button onClick={() => setGroupedData({})}>clear</button>
+
+      <ResponsiveContainer width="100%" height={400} >
+        <LineChart margin={{ "top": 25, "bottom": 25, "left": 25, "right": 25 }} data={groupedData['sensor1']?.data ?? [null]}>
+          <Line name="sensor1" type="linear" dataKey="data" stroke="#8884d8" dot={false} activeDot={true} isAnimationActive={false} />
+          <XAxis dataKey="elapsedTime">
+            <Label value="Time elapsed (s)" offset={10} position="bottom" />
+          </XAxis>
+          <YAxis dataKey="data" />
+          <Legend layout="vertical" align="right" verticalAlign="top" />
+        </LineChart>
+      </ResponsiveContainer>
+
+      <ResponsiveContainer width="100%" height={400} >
+        <LineChart margin={{ "top": 25, "bottom": 25, "left": 25, "right": 25 }} data={groupedData['sensor2']?.data ?? [null]}>
+          <Line name="sensor2" type="linear" dataKey="data" stroke="#8884d8" dot={false} activeDot={true} isAnimationActive={false} />
+          <XAxis dataKey="elapsedTime">
+            <Label value="Time elapsed (s)" offset={10} position="bottom" />
+          </XAxis>
+          <YAxis dataKey="data" />
+          <Legend layout="vertical" align="right" verticalAlign="top" />
+        </LineChart>
+      </ResponsiveContainer>
+
+      {/* <pre>{JSON.stringify(groupedData['sensor1'], null, 2)}</pre> */}
     </>
   )
 }
