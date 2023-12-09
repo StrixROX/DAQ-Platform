4de4cac80370741bf493932cd548d2ffb2b8b5c3
StrixROX <40167899+StrixROX@users.noreply.github.com>
2023-12-09 10:50:52 +0530

    Created DownloadDataButton component

---
+++ src/components/DownloadDataButton.js
@@ -0,0 +1,36 @@
+import { useCallback } from 'react'
+
+function generateCSVBlob(sensorId, data) {
+  let out = [`Time (s), Elapsed Time (s), ${sensorId}\n`]
+
+  for (const el of data) {
+    out.push(`${el.time}, ${el.elapsedTime}, ${el.data}\n`)
+  }
+
+  return new Blob(out, { type: "text/csv" })
+}
+
+export default function DownloadDataButton({ sensorID, data }) {
+  if (!data) {
+    return null
+  }
+
+  const filename = `${sensorID}.csv`
+
+  const downloadData = useCallback((sensorId, data) => {
+    const blob = generateCSVBlob(sensorId, data)
+
+    const el = document.createElement('a')
+    el.setAttribute('href', URL.createObjectURL(blob))
+    el.setAttribute('download', filename)
+    el.click()
+    el.remove()
+
+  }, [])
+
+  return (
+    <button onClick={() => downloadData(sensorID, data)}>
+      Download {filename}
+    </button>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,25 +3,11 @@ import io from 'socket.io-client'
 
 import styles from '@/styles/Home.module.css'
 
-import Plots from '@/components/Plots'
 import { useSensorData, useUpdateSensorData, useClearSensorData } from '@/context/SensorDataContext'
-
-function validate(data) {
-  return typeof (data?.time) === 'number' && typeof (data?.data) === 'number' && typeof (data?.sensorId) === 'string'
-}
-
-function generateBlob(sensorId, data) {
-  let out = [`Time (s), Elapsed Time (s), ${sensorId}\n`]
-
-  for (const el of data) {
-    out.push(`${el.time}, ${el.elapsedTime}, ${el.data}\n`)
-  }
-
-  return new Blob(out, { type: "text/csv" })
-}
+import Plots from '@/components/Plots'
+import DownloadDataButton from '@/components/DownloadDataButton'
 
 let socket = null
-const windowSize = 20
 
 export default function Home() {
   const [connectionStatus, setConnectionStatus] = useState("Not connected")
@@ -55,16 +41,6 @@ export default function Home() {
 
   useEffect(() => connect(), [])
 
-  const downloadData = useCallback((sensorId, data) => {
-    const blob = generateBlob(sensorId, data)
-
-    const el = document.createElement('a')
-    el.setAttribute('href', URL.createObjectURL(blob))
-    el.setAttribute('download', `${sensorId}.csv`)
-    el.click()
-    el.remove()
-  }, [])
-
   return (
     <>
       <h1 className={styles.heading}>DAQ Platform</h1>
@@ -72,13 +48,11 @@ export default function Home() {
       <button onClick={() => socket?.disconnect() || connect()}>connect/disonnect</button>
       <button onClick={() => clearSensorData()}>clear</button>
 
-      {Object.keys(sensorData).map((sensorId, i) => {
-        return (
-          <button onClick={() => downloadData(sensorId, sensorData[sensorId].data)} key={i}>
-            Download {sensorId}.csv
-          </button>
-        )
-      })}
+      {
+        Object.keys(sensorData).map((sensorId, i) => {
+          return <DownloadDataButton sensorID={sensorId} data={sensorData[sensorId].data} key={i} />
+        })
+      }
 
       <Plots />
 
